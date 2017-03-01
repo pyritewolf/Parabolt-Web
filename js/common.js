5c4dabbc448d185f5006fbe22363fda0fed711f5
Julia Morelli <jmlanghe@gmail.com>
2017-03-01 15:11:32 -0300

    Press key to scroll
    + agregado JS para que baje de section en section apretando las flechas
    para arriba y para abajo

--- js/common.js
+++ js/common.js
@@ -10,6 +10,58 @@ $(document).ready(function () {
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
   });
+
+
+
+
+
+  //press arrow to slide
+  $(document).keydown(function(e) {
+
+    var currentScroll = $(document).scrollTop();
+    var i = 0;
+    var sectNumber = 0;
+    $("section").each(function () {
+      if (Math.floor($(this).offset().top) <= currentScroll) {
+        i++;
+      } else {
+        if (i == 0) { i = 1; }
+        sectNumber = i;
+      }
+    });
+
+      if (e.which == 39 || e.which == 40) {
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $("section:nth-of-type("+ (sectNumber+1)+")").offset().top
+        }, 500);
+        $("side a").removeClass("selected");
+        $("side li:nth-of-type("+(sectNumber+1)+") a").addClass("selected");
+      } else if ((e.which == 37 || e.which == 38) && sectNumber != i) {
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $("section:nth-of-type("+ (i-1)+")").offset().top
+        }, 500);
+        $("side a").removeClass("selected");
+        $("side li:nth-of-type("+(i-1)+") a").addClass("selected");
+      } else if ((e.which == 37 || e.which == 38) && sectNumber != 1) {
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $("section:nth-of-type("+ (sectNumber-1)+")").offset().top
+        }, 500);
+        $("side a").removeClass("selected");
+        $("side li:nth-of-type("+(sectNumber-1)+") a").addClass("selected");
+      } else if ((e.which == 37 || e.which == 38) && sectNumber == 1) {
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 500);
+        $("side a").removeClass("selected");
+        $("side li:nth-of-type(1) a").addClass("selected");
+      }
+
+  });
+
 });
 
 $(window).resize(function () {
